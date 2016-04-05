2bb243852f52c25f39419bd6dfa888659a9b724d
julianhatwell <julian.hatwell@googlemail.com>
2016-04-05 17:46:16 +0800

    simple

---
+++ simpleTheme.R
@@ -0,0 +1,15 @@
+# Simple Theme
+# Simple Lattice Theme
+MyLatticeFont <- list(font = 1, cex = 1)
+MyLatticeTheme <- list(
+  par.main.text = MyLatticeFont
+  , par.xlab.text = MyLatticeFont
+  , par.ylab.text = MyLatticeFont
+  , axis.text = MyLatticeFont
+  , fontsize = list(text = 11, points = 7)
+  , plot.symbol = list(pch = 19, alpha = 0.5, cex = 0.75)
+  , box.umbrella = list(col = "black", lty = 2, lwd = 1.25)
+  , box.rectangle = list(col = "black", lwd = 1.5)
+  , box.dot = list(pch = 15, cex = 0.8, alpha = 0.5)
+  , strip.background = list(col="transparent")
+)
