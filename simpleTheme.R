467d201d6a38754518264248bf08fe24f2fc981a
julianhatwell <julian.hatwell@googlemail.com>
2016-04-07 12:50:08 +0800

    simple

--- simpleTheme.R
+++ simpleTheme.R
@@ -9,10 +9,10 @@ MyLatticeTheme <- list(
   , par.ylab.text = MyLatticeFont
   , axis.text = MyLatticeFont
   , fontsize = list(text = 11, points = 7)
-  , plot.symbol = list(pch = 19, alpha = 0.5, cex = 0.75)
+  , plot.symbol = list(col = "#44a4ff", pch = 19, alpha = 0.5, cex = 0.75)
   , box.umbrella = list(col = "black", lty = 2, lwd = 1.25)
   , box.rectangle = list(col = "black", lwd = 1.5)
-  , box.dot = list(pch = 15, cex = 0.8, alpha = 0.5)
+  , box.dot = list(col = "#44a4ff", pch = 15, cex = 0.8, alpha = 0.5)
   , plot.line = list(col = c("grey40", "darkblue", "violetred2"))
   , add.line = list(col = "grey70", lty = 2)
   , strip.background = list(col="transparent")
