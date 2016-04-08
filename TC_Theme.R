e2f6641b8c6f67f0c5d35db9dad7662b6b63cd6d
julianhatwell <julian.hatwell@googlemail.com>
2016-04-08 13:03:20 +0800

    TC

--- TC_Theme.R
+++ TC_Theme.R
@@ -1,13 +1,18 @@
 # Theme for TC report
 # a nice colour palette
 myPal <- c("#8DD3C7", "#B0A8B3", "#9FB5D6", "#9EC0FA", "#DB8072")
-myPalDark <- c("#4D8377", "#504853", "#3F5576", "#3E409A", "#7B2012")
+myPalDark <- c("#4D8377", "#504853", "#3F5576", "#3E40DA", "#AB2012")
 myPalContrasts <- c(myPalDark[1], myPalDark[5], myPal[2], myPal[4]
                     ,"#999999"
                     , myPal[1], myPal[5], myPalDark[2], myPalDark[4])
 myPal.range <- colorRampPalette(c("#FFFFFF", myPal[3:1]))
 myPal.rangeDark <- colorRampPalette(c("#FFFFFF", myPalDark[3:1]))
 myPal.rangeDiv <- colorRampPalette(c(myPal[1], "#FFFFFF", myPal[5]))
+
+# fourfold 6
+myPalFourFold <- c(myPalContrasts[6], myPalContrasts[1]
+                   , myPalContrasts[7], myPalContrasts[4]
+                   , myPalContrasts[2], myPalContrasts[9])
 
 # applied to lattice
 MyLatticeFont <- list(font = 1, cex = 1, col = myPalDark[2])
